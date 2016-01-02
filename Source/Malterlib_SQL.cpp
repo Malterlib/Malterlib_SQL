60b9ed31feb19f9b0d4fcefe921a94e611c742b1
Erik Olofsson <erik.olofsson@hansoft.com>
2016-01-02 09:39:40 +0000

    [Malterlib] Update license
    [grafted history before @55251]
    
    Copied from Perforce
     Change: 55250

---
+++ Source/Malterlib_SQL.cpp
@@ -0,0 +1,460 @@
+﻿// Copyright © 2015 Hansoft AB 
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+#include "Malterlib_SQL.h"
+#include <Mib/Core/RuntimeType>
+
+using namespace NMib::NStr;
+using namespace NMib::NPtr;
+
+namespace NMib
+{
+    namespace NSQL
+	{
+		CQuery::CQuery()
+		{
+		}
+		
+		CQuery::~CQuery()
+		{
+		}
+		
+		bint CQueryInstance::f_BindParameterNull(int _iParam)
+		{
+			return f_BindParameter(_iParam, 0, 0);
+		}
+		
+		CTransactionResult::~CTransactionResult()
+		{
+			m_Results.f_DeleteAll();
+		}
+		
+		CQueryResult *CTransactionResult::f_GetQueryResult(aint _iQuery)
+		{
+			if (_iQuery < 0 || (mint)_iQuery >= m_Results.f_GetLen())
+				return nullptr;
+			return m_Results[_iQuery];
+		}
+		
+
+		CTransaction::CQueryLink::CQueryLink()
+		{
+			m_pQueryInstance = nullptr;
+		}
+		
+		CTransaction::CQueryLink::~CQueryLink()
+		{
+			if (m_pQueryInstance)
+				m_pQueryInstance->f_Delete();
+		}
+		
+		CTransactionHandler::~CTransactionHandler()
+		{
+		}
+
+		void CTransactionHandler::fs_HandleTransaction(CTransactionResult *_pResult, void *_pContext)
+		{
+			((CTransactionHandler *)_pContext)->f_HandleTransaction(_pResult);
+		}
+
+
+		CSQLConnection::CTask::CTask(CSQLConnection *_pSQL)
+		{
+			m_pSQL = _pSQL;
+			m_pTransaction = nullptr;
+			m_pContext = nullptr;
+			m_pEvent = nullptr;
+			m_fCallback = nullptr;
+			m_bAsync = false;
+			m_pResult = nullptr;
+		}
+
+		CSQLConnection::CTask::~CTask()
+		{
+			if (m_pResult)
+				delete m_pResult;
+		}
+		
+		void CSQLConnection::fp_QueueTask(CTask *_pTask)
+		{
+			DMibLockTyped(NThread::CMutual, mp_QueueLock);
+			mp_QueuedTasks.f_Insert(_pTask);
+			if (_pTask->m_pEvent)
+				_pTask->m_pEvent->f_Signal();
+
+		}
+
+		CTransactionResult *CSQLConnection::fp_CommitTransaction(CTransaction *_pTransaction, CDatabaseImplementation *_pImp)
+		{
+			_pImp->f_BeginTransaction();
+
+			CTransactionResult *pResult = DMibNew CTransactionResult;
+			pResult->m_Results.f_SetLen(_pTransaction->m_nTransactions);
+			NMem::fg_ObjectSet(pResult->m_Results.f_GetArray(), (CQueryResult *)nullptr, _pTransaction->m_nTransactions);
+			int iResult = 0;
+			CTransaction::CQueryIter Iter = _pTransaction->m_Transactions;
+
+			while (Iter)
+			{
+				CQueryResult * pRes = _pImp->f_RunQuery(Iter->m_pQueryInstance);
+				if (!pRes && !_pTransaction->m_bAllowFail)
+				{
+					delete pResult;
+					_pImp->f_RollbackTransaction();
+					return nullptr;
+				}
+				pResult->m_Results[iResult++] = pRes;
+				
+				++Iter;
+			}
+
+			_pImp->f_CommitTransaction();
+
+			return pResult;
+		}
+		
+		
+		NStr::CStr CSQLConnection::CWorkerThread::f_GetThreadName()
+		{
+			return "Malterlib_DBConnWorkerThread";
+		}
+
+		CSQLConnection::CWorkerThread::CWorkerThread(CSQLConnection *_pSQL)
+		{
+			m_pSQLConnection = _pSQL;
+			f_Start();
+			m_pTask = nullptr;
+		}
+
+		CSQLConnection::CWorkerThread::~CWorkerThread()
+		{
+			f_Stop();
+		}
+
+		aint CSQLConnection::CWorkerThread::f_Main()
+		{
+			m_EventWantQuit.f_ReportTo(&m_Event);
+			auto *pImp = NMib::fg_CreateRuntimeType<CDatabaseImplementation>(m_pSQLConnection->mp_Implementation);
+
+			if (pImp)
+			{
+				if (pImp->f_Create(m_pSQLConnection->mp_Parameters))
+				{
+					while (f_GetState() != NThread::EThreadState_EventWantQuit)
+					{
+						if (m_pTask)
+						{
+							m_pTask->f_Run(pImp);
+							m_pTask = nullptr;
+							{
+								DMibLock(m_pSQLConnection->mp_ThreadsLock);
+								m_pSQLConnection->mp_FreeThreads.f_Insert(this);
+								m_pSQLConnection->mp_ThreadEvent.f_Signal();
+							}
+						}
+						m_Event.f_Wait();
+					}
+				}
+
+				pImp->f_Destroy(true);
+				delete pImp;
+			}
+
+			{
+				DMibLock(m_pSQLConnection->mp_ThreadsLock);
+				m_pSQLConnection->mp_DeletedThreads.f_Insert(this);
+				m_pSQLConnection->mp_ThreadEvent.f_Signal();
+			}
+
+			return 0;
+		}
+
+		CSQLConnection::CSQLConnection()
+		{
+			mp_pMainImp = nullptr;
+		}
+
+		CSQLConnection::~CSQLConnection()
+		{
+			f_Close();
+		}
+
+		void CSQLConnection::CTask::f_Run(CDatabaseImplementation *_pImp)
+		{
+			CTransactionResult *pResult = m_pSQL->fp_CommitTransaction(m_pTransaction, _pImp);
+			if (m_bAsync)
+			{
+				m_fCallback(pResult, m_pContext);
+				delete pResult;
+				delete this;
+				return;
+			}
+			m_pResult = pResult;
+			m_pSQL->fp_QueueTask(this);
+			//_pImp->f_CreateQuery()
+		}
+
+		bint CSQLConnection::f_Create(const ch8 *_pImplementation, const NRegistry::CRegistry_CStr &_Parameters, mint _nWorkerThreads)
+		{
+			if (mp_pMainImp)
+			{
+				mp_pMainImp->f_Destroy(false);
+				delete mp_pMainImp;
+			}
+			mp_Implementation = _pImplementation;
+			mp_Parameters = _Parameters;			
+
+			mp_pMainImp = NMib::fg_CreateRuntimeType<CDatabaseImplementation>(mp_Implementation);
+			if (!mp_pMainImp)
+				DMibErrorDatabase(NStr::CStrNonTracked::CFormat("Could not create database implementation {}") << mp_Implementation);
+
+			if (!mp_pMainImp->f_Create(mp_Parameters))
+				return false;
+
+			for (mint i = 0; i < _nWorkerThreads; ++i)
+			{
+				CWorkerThread *pThread = DMibNew CWorkerThread(this);
+				mp_FreeThreads.f_Insert(pThread);
+			}
+			return true;
+		}
+
+		void CSQLConnection::f_Close()
+		{
+			{
+				DMibLock(mp_ThreadsLock);
+				while (mp_WorkingThreads.f_GetFirst())
+				{
+					CWorkerThread *pThread = mp_WorkingThreads.f_GetFirst();
+					
+					{
+						DMibUnlock(mp_ThreadsLock);
+						pThread->f_Stop();
+					}
+				}
+				while (mp_FreeThreads.f_GetFirst())
+				{
+					CWorkerThread *pThread = mp_FreeThreads.f_GetFirst();
+					
+					{
+						DMibUnlock(mp_ThreadsLock);
+						pThread->f_Stop();
+					}
+				}
+			}
+			mp_DeletedThreads.f_DeleteAll();
+
+			if (mp_pMainImp)
+			{
+				mp_pMainImp->f_Destroy(false);
+				delete mp_pMainImp;
+				mp_pMainImp = nullptr;
+			}
+		}
+
+
+		CQueryResult *CSQLConnection::f_ExecuteQuery(const NStr::CStr &_Query, bool _bTransaction)
+		{
+			CQuery *pQuery = f_CreateQuery(_Query);
+			if (!pQuery)
+				return nullptr;
+			CQueryInstance *pInst = pQuery->f_CreateQueryInstance();
+			if (!pInst)
+			{
+				pQuery->f_Delete();
+				return nullptr;
+			}
+			if (_bTransaction)
+				mp_pMainImp->f_BeginTransaction();
+			CQueryResult *pRes = mp_pMainImp->f_RunQuery(pInst);
+			pInst->f_Delete();
+			pQuery->f_Delete();
+			if (!pRes)
+			{
+				if (_bTransaction)
+					mp_pMainImp->f_RollbackTransaction();
+				return nullptr;
+			}
+			if (_bTransaction)
+				mp_pMainImp->f_CommitTransaction();
+			return pRes;
+		}
+
+		CQueryResult *CSQLConnection::f_ExecuteQuery(CQuery *_pQuery)
+		{
+			TCUniquePointer<CQueryInstance>pInst = fg_Explicit(_pQuery->f_CreateQueryInstance());
+			if (!pInst)
+				return nullptr;
+
+			mp_pMainImp->f_BeginTransaction();
+			CQueryResult *pRes = mp_pMainImp->f_RunQuery(pInst.f_Get());
+			pInst = 0;
+
+			if (!pRes)
+			{
+				mp_pMainImp->f_RollbackTransaction();
+				return nullptr;
+			}
+
+			mp_pMainImp->f_CommitTransaction();
+			return pRes;
+		}
+
+
+		CQueryResult *CSQLConnection::f_ExecuteQuery(CQueryInstance *_pQueryInst)
+		{
+			mp_pMainImp->f_BeginTransaction();
+			CQueryResult *pRes = mp_pMainImp->f_RunQuery(_pQueryInst);
+			if (!pRes)
+			{
+				mp_pMainImp->f_RollbackTransaction();
+				return nullptr;
+			}
+			mp_pMainImp->f_CommitTransaction();
+			return pRes;
+		}
+
+		CTransactionResult *CSQLConnection::f_CommitTransaction(CTransaction *_pTransaction)
+		{
+			return fp_CommitTransaction(_pTransaction, mp_pMainImp);
+		}
+
+		// Async transactions
+		void CSQLConnection::f_CommitTransaction(CTransaction *_pTransaction, void * _pContext, NThread::CSemaphoreReportableAggregate *_pEvent, PFTransactionResultCallback *_fCallback, bint _bAsync)
+		{
+			CWorkerThread *pThread = nullptr;
+			while (!pThread)
+			{
+				{
+					DMibLock(mp_ThreadsLock);
+					pThread = mp_FreeThreads.f_Pop();
+					if (pThread)
+						mp_WorkingThreads.f_Insert(pThread);
+				}
+				if (!pThread)
+					mp_ThreadEvent.f_Wait();
+			}
+
+			CTask *pTask = DMibNew CTask(this);
+			pTask->m_pTransaction = _pTransaction;
+			pTask->m_pContext = _pContext;
+			pTask->m_pEvent = _pEvent;
+			pTask->m_fCallback = _fCallback;
+			pTask->m_bAsync = _bAsync;
+			pThread->m_pTask = pTask;
+			pThread->m_Event.f_Signal();
+		}
+
+		void CSQLConnection::f_CommitTransaction(CTransaction *_pTransaction, CTransactionHandler *_pHandler, NThread::CSemaphoreReportableAggregate *_pEvent, bint _bAsync)
+		{
+			f_CommitTransaction(_pTransaction, _pHandler, _pEvent, CTransactionHandler::fs_HandleTransaction, _bAsync);
+		}
+
+		void CSQLConnection::f_ProcessTansactions()
+		{
+			int nTasks = 0;
+			{
+				DMibLockTyped(NThread::CMutual, mp_QueueLock);
+				nTasks = mp_QueuedTasks.f_GetLen();
+			}
+
+			while (nTasks)
+			{
+				CTask *pTask;
+				{
+					DMibLockTyped(NThread::CMutual, mp_QueueLock);
+					pTask = mp_QueuedTasks.f_Pop();
+				}
+
+				if (!pTask)
+					break;
+
+				pTask->m_fCallback(pTask->m_pResult, pTask->m_pContext);
+				delete pTask;
+			}
+		}
+
+		CTransaction *CSQLConnection::f_CreateTransaction()
+		{
+			return DMibNew CTransaction;
+
+		}
+
+		CTransaction *CSQLConnection::f_CreateTransaction(CStr const& _Statements)	// Create a transaction from a list of semi-colon separated statements.
+		{
+			TCUniquePointer<CTransaction> pTransaction = fg_Construct();
+
+			CStr Statements = _Statements;
+			CStr CurStatement;
+			
+			TCUniquePointer<CQuery> pQuery;
+			
+			while (!Statements.f_IsEmpty())
+			{
+				CurStatement = fg_GetStrSep(Statements, ";");
+				CurStatement = CurStatement.f_Trim();
+
+				pQuery = fg_Explicit(f_CreateQuery(CurStatement));
+				if (!pQuery)
+					return nullptr;
+				
+				pTransaction->f_AddQuery(pQuery.f_Get());
+				pTransaction->m_OwnedQueries.f_Insert() = fg_Explicit(pQuery.f_Detach());				
+			}
+
+			return pTransaction.f_Detach();
+		}
+
+		CQuery *CSQLConnection::f_CreateQuery(const NStr::CStr &_Query)
+		{
+            return mp_pMainImp->f_CreateQuery(_Query);
+		}
+
+		CQueryInstance *CSQLConnection::f_CreateQueryInstance(CQuery* _pQuery)
+		{
+			return _pQuery->f_CreateQueryInstance();
+		}
+
+		CTransaction::CTransaction()
+		{
+			m_nTransactions = 0;
+			m_bAllowFail = false;
+		}
+
+		CTransaction::~CTransaction()
+		{
+			m_Transactions.f_DeleteAll();
+		}
+
+		CQueryInstance *CTransaction::f_AddQuery(CQuery *_pQuery)
+		{
+			++m_nTransactions;
+			CQueryLink *pLink = DMibNew CQueryLink;
+			pLink->m_pQueryInstance = _pQuery->f_CreateQueryInstance();
+			m_Transactions.f_Insert(pLink);
+			return pLink->m_pQueryInstance;
+		}
+
+		void CTransaction::f_Delete()
+		{
+			delete this;
+		}
+
+		void CTransaction::f_SetAllowFail(bint _bAllow)
+		{
+			m_bAllowFail = _bAllow;
+		}
+
+//		DMibRuntimeClassBaseNamed(CDatabaseImplementation, NMib::NSQL::CDatabaseImplementation);
+
+		/*
+		CQuery *CSQLConnection::f_CreateQuery(const NStr::CStr &_Query);
+		CTransaction *CSQLConnection::f_CreateTransaciton();
+		CQueryInstance *CSQLConnection::f_CreateQueryInstance(CQuery *_pQuery);
+
+		// Async transactions
+		void CSQLConnection::f_CommitTransaction(CTransaction *_pTransaction, void * _pContext, NThread::CSemaphoreReportableAggregate *_pEvent, PFTransactionResultCallback *_fCallback);
+		CTransactionResult *CSQLConnection::f_GetTransacitonResult(void * &_pContext);
+*/
+	}
+}
---
+++ Test/Test_Malterlib_SQL.cpp
@@ -0,0 +1,223 @@
+// Copyright © 2015 Hansoft AB 
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+#include <Mib/SQL/SQL>
+
+using namespace NMib::NStr;
+using namespace NMib::NPtr;
+using namespace NMib::NRegistry;
+
+using NMib::fg_Explicit;
+
+void fg_Malterlib_SQL_MySql_MakeActive();
+void fg_Malterlib_SQL_SQLite_MakeActive();
+
+namespace 
+{
+	class CDB_Tests : public NMib::NTest::CTest
+	{
+	public:
+
+		enum EFlag
+		{
+			EFlag_None			= 0,
+			EFlag_NoBinding		= DMibBit(0),
+			EFlag_MySqlSyntax	= DMibBit(1),
+		};
+		
+		void f_DoSQLConnectionTests(NMib::NSQL::CSQLConnection &_SQLConn, EFlag _Flags)
+		{
+			NMib::NSQL::CSQLConnection &SQLConn = _SQLConn;
+
+			// Test CREATE TABLE
+			TCUniquePointer<NMib::NSQL::CQueryResult> pResults = fg_Explicit(SQLConn.f_ExecuteQuery("CREATE TABLE TestTable (P_Id INTEGER, Name TEXT, PRIMARY KEY (P_Id) )"));
+			DMibTest(DMibExpr(pResults)) (ETest_FailAndStop);
+
+			// Test INSERT
+			for (int i= 0; i < 100; ++i)
+			{
+				pResults =	fg_Explicit(SQLConn.f_ExecuteQuery(CStr(CStr(CStr::CFormat("INSERT INTO TestTable VALUES ({}, '{}')") << i << i))));
+				DMibTest(DMibExpr(pResults) && DMibExpr(2)) (ETest_FailAndStop) (ETestFlag_Aggregated);
+				//					DMibTest(DMibExpr(pResults->f_NumAffectedRows()) == DMibExpr(1)) (ETest_FailAndStop); // SQLite is not obeying the docs here.
+			}
+
+			// Test Select
+			pResults =	fg_Explicit(SQLConn.f_ExecuteQuery("SELECT * FROM TestTable WHERE P_Id < 50 ORDER BY P_Id ASC"));
+			DMibTest(DMibExpr(!pResults.f_IsEmpty())) (ETest_FailAndStop);
+			DMibTest(DMibExpr(pResults->f_NumReturnedRows()) == DMibExpr(50)) (ETest_FailAndStop);
+
+			int64 Val, Req;
+			for (int64 i= 0; i < 50; ++i)
+			{
+				Req = i;
+				Val = pResults->f_GetInt64(i, 0);
+				DMibTest(DMibExpr(Val) == DMibExpr(Req)) (ETest_FailAndStop) (ETestFlag_Aggregated);
+			}
+
+			// Test DELETE
+			pResults =	fg_Explicit(SQLConn.f_ExecuteQuery("DELETE FROM TestTable WHERE P_Id < 50"));
+			DMibTest(DMibExpr(!pResults.f_IsEmpty()) && DMibExpr(2)) (ETest_FailAndStop);
+			//				DMibTest(DMibExpr(pResults->f_NumAffectedRows()) == DMibExpr(50)) (ETest_FailAndStop);
+
+			// Test results of DELETE:
+			pResults =	fg_Explicit(SQLConn.f_ExecuteQuery("SELECT * FROM TestTable ORDER BY P_Id ASC"));
+			DMibTest(DMibExpr(!pResults.f_IsEmpty()) && DMibExpr(3)) (ETest_FailAndStop);
+			DMibTest(DMibExpr(pResults->f_NumReturnedRows()) == DMibExpr(50) && DMibExpr(2)) (ETest_FailAndStop);
+
+			for (int64 i= 0; i < 50; ++i)
+			{
+				Req = i + 50;
+				Val = pResults->f_GetInt64(i, 0);
+				DMibTest(DMibExpr(Val) == DMibExpr(Req) && DMibExpr(2)) (ETest_FailAndStop) (ETestFlag_Aggregated);
+			}
+
+			// Test Queries with bound params.
+			if (!(_Flags & EFlag_NoBinding))
+			{
+				DMibTestPath("Bound params");
+				TCUniquePointer<NMib::NSQL::CQuery> pParamQuery = fg_Explicit(SQLConn.f_CreateQuery("SELECT * FROM TestTable WHERE P_Id < ? ORDER BY P_Id ASC"));
+				DMibTest(DMibExpr(pParamQuery)) (ETest_FailAndStop);
+
+				TCUniquePointer<NMib::NSQL::CQueryInstance> pQInst = fg_Explicit(SQLConn.f_CreateQueryInstance(pParamQuery.f_Get()));
+				DMibTest(DMibExpr(pQInst)) (ETest_FailAndStop);
+
+				DMibTest(DMibExpr(pQInst->f_BindParameter<int32>(0, 75)) > DMibExpr(0)) (ETest_FailAndStop);
+
+				pResults =	fg_Explicit(SQLConn.f_ExecuteQuery(pQInst.f_Get()));
+				DMibTest(DMibExpr(!pResults.f_IsEmpty())) (ETest_FailAndStop);
+				DMibTest(DMibExpr(pResults->f_NumReturnedRows()) == DMibExpr(25)) (ETest_FailAndStop);
+				for (int64 i= 0; i < 25; ++i)
+				{
+					Req = i + 50;
+					Val = pResults->f_GetInt64(i, 0);
+					DMibTest(DMibExpr(Val) == DMibExpr(Req)) (ETest_FailAndStop) (ETestFlag_Aggregated);
+				}
+			}
+
+			// Test failing transaction
+			{
+				DMibTestPath("Failing transaction");
+				TCUniquePointer<NMib::NSQL::CTransaction> pTransaction = fg_Explicit(SQLConn.f_CreateTransaction());
+				DMibTest(DMibExpr(pTransaction)) (ETest_FailAndStop);
+
+				// Transaction adding one entry and then deleting one non-existant entry.
+
+
+				TCUniquePointer<NMib::NSQL::CQuery> pQueryOne = fg_Explicit(SQLConn.f_CreateQuery("INSERT INTO TestTable VALUES (500, '500')"));
+				DMibTest(DMibExpr(pQueryOne)) (ETest_FailAndStop);
+
+				DMibTest(DMibExpr(pTransaction->f_AddQuery(pQueryOne.f_Get()))) (ETest_FailAndStop);
+
+				TCUniquePointer<NMib::NSQL::CQuery> pQueryTwo = (_Flags & EFlag_MySqlSyntax) ? fg_Explicit(SQLConn.f_CreateQuery("INSERT INTO TestTable VALUES (500, 'Failure')")) : fg_Explicit(SQLConn.f_CreateQuery("INSERT OR FAIL INTO TestTable VALUES (500, 'Failure')"));
+				DMibTest(DMibExpr(pQueryTwo)) (ETest_FailAndStop);
+
+				DMibTest(DMibExpr(pTransaction->f_AddQuery(pQueryTwo.f_Get()))) (ETest_FailAndStop);
+
+				TCUniquePointer<NMib::NSQL::CTransactionResult> pTransactionResult = fg_Explicit(SQLConn.f_CommitTransaction(pTransaction.f_Get()));
+				DMibTest(DMibExpr(!pTransactionResult)) (ETest_FailAndStop);
+
+				//					pTransactionResult->
+
+				// Test results of Rolledback transaction:
+				pResults =	fg_Explicit(SQLConn.f_ExecuteQuery("SELECT * FROM TestTable WHERE P_Id = 500"));
+				DMibTest(DMibExpr(!pResults.f_IsEmpty())) (ETest_FailAndStop);
+				DMibTest(DMibExpr(pResults->f_NumReturnedRows()) == DMibExpr(0)) (ETest_FailAndStop);
+			}
+
+			// Test successful transaction
+			{
+				DMibTestPath("Successful transaction");
+				TCUniquePointer<NMib::NSQL::CTransaction> pTransaction = fg_Explicit(SQLConn.f_CreateTransaction());
+				DMibTest(DMibExpr(pTransaction)) (ETest_FailAndStop);
+
+				// Transaction adding one entry and then deleting one non-existant entry.
+
+
+				TCUniquePointer<NMib::NSQL::CQuery> pQueryOne = fg_Explicit(SQLConn.f_CreateQuery("INSERT INTO TestTable VALUES (500, '500')"));
+				DMibTest(DMibExpr(pQueryOne)) (ETest_FailAndStop);
+
+				DMibTest(DMibExpr(pTransaction->f_AddQuery(pQueryOne.f_Get()))) (ETest_FailAndStop);
+
+
+				TCUniquePointer<NMib::NSQL::CQuery> pQueryTwo = (_Flags & EFlag_MySqlSyntax) ? fg_Explicit(SQLConn.f_CreateQuery("REPLACE INTO TestTable VALUES (500, 'Success')")) : fg_Explicit(SQLConn.f_CreateQuery("INSERT OR REPLACE INTO TestTable VALUES (500, 'Success')"));
+				DMibTest(DMibExpr(pQueryTwo)) (ETest_FailAndStop);
+
+				DMibTest(DMibExpr(pTransaction->f_AddQuery(pQueryTwo.f_Get()))) (ETest_FailAndStop);
+
+				TCUniquePointer<NMib::NSQL::CTransactionResult> pTransactionResult = fg_Explicit(SQLConn.f_CommitTransaction(pTransaction.f_Get()));
+				DMibTest(DMibExpr(pTransactionResult)) (ETest_FailAndStop);
+
+				//					pTransactionResult->
+
+				// Test results of Rolledback transaction:
+				pResults =	fg_Explicit(SQLConn.f_ExecuteQuery("SELECT * FROM TestTable WHERE P_Id = 500"));
+				DMibTest(DMibExpr(!pResults.f_IsEmpty()) && DMibExpr(6)) (ETest_FailAndStop);
+				DMibTest(DMibExpr(pResults->f_NumReturnedRows()) == DMibExpr(1)) (ETest_FailAndStop);
+
+				int64 ID = pResults->f_GetInt64(0, 0);
+				int64 ReqID = 500;
+				DMibTest(DMibExpr(ID) == DMibExpr(ReqID));
+
+				CStr MsgStr = pResults->f_GetString(0, 1);
+				CStr ReqMsgStr = "Success";
+				DMibTest(DMibExpr(MsgStr) == DMibExpr(ReqMsgStr));
+			}
+		}
+
+		void f_DoTests()
+		{
+			
+			///*
+			DMibTestSuite("SQLite")
+			{
+				fg_Malterlib_SQL_SQLite_MakeActive();
+				
+				NMib::NSQL::CSQLConnection SQLConn;
+
+				// Test create DB (in memory)
+				CRegistry_CStr Registry;
+				Registry.f_SetValue("Database", ":memory:");
+				DMibTest(DMibExpr(SQLConn.f_Create("NMib::NSQL::CDatabaseImplementation_SQLite", Registry, 1))) (ETest_FailAndStop);
+
+				f_DoSQLConnectionTests(SQLConn, EFlag_None);
+			};
+			//*/
+
+#ifdef HAVE_YOU_SET_UP_A_LOCALHOST_MYSQL_SERVER
+			///*
+			DMibTestSuite("MySQL")
+			{
+				fg_Malterlib_SQL_MySql_MakeActive();
+				NMib::NSQL::CSQLConnection SQLConn;
+
+				// Connect to local db.
+				CRegistry_CStr Registry;
+				Registry.f_SetValue("Host", "localhost");
+				Registry.f_SetValue("User", "root");
+				Registry.f_SetValue("Password", "asdasd");
+				DMibTest(DMibExpr(SQLConn.f_Create("NMib::NSQL::CDatabaseImplementation_MySql", Registry, 1))) (ETest_FailAndStop);
+
+				TCUniquePointer<NMib::NSQL::CQueryResult> pResults;
+
+				pResults =	fg_Explicit(SQLConn.f_ExecuteQuery("CREATE DATABASE MalterlibCertifierTestDB"));
+				DMibTest(DMibExpr(!pResults.f_IsEmpty())) (ETest_FailAndStop);
+				DMibTest(DMibExpr(pResults->f_NumReturnedRows()) == DMibExpr(0)) (ETest_FailAndStop);
+
+				pResults =	fg_Explicit(SQLConn.f_ExecuteQuery("USE MalterlibCertifierTestDB"));
+				DMibTest(DMibExpr(!pResults.f_IsEmpty()) && DMibExpr(2)) (ETest_FailAndStop);
+				DMibTest(DMibExpr(pResults->f_NumReturnedRows()) == DMibExpr(0) && DMibExpr(2)) (ETest_FailAndStop);
+
+				f_DoSQLConnectionTests(SQLConn, EFlag_NoBinding | EFlag_MySqlSyntax);
+
+				pResults =	fg_Explicit(SQLConn.f_ExecuteQuery("DROP DATABASE MalterlibCertifierTestDB"));
+				DMibTest(DMibExpr(!pResults.f_IsEmpty()) && DMibExpr(3)) (ETest_FailAndStop);
+				DMibTest(DMibExpr(pResults->f_NumReturnedRows()) == DMibExpr(0) && DMibExpr(3)) (ETest_FailAndStop);
+			};
+			//*/
+#endif
+
+		}
+
+	};
+}
+DMibTestRegister(CDB_Tests, Malterlib::SQL);
