930495da6acf7a05e64fa8ab15ca3b485366d6c9
Erik Olofsson <erik@olofsson.info>
2024-05-12 20:13:10 +0200

    [Malterlib/SQL] Update import cache

--- ImportCache/External_mariadb-connector-c-macOS-x64-Release/mariadb_config/mariadb_config.c
+++ ImportCache/External_mariadb-connector-c-macOS-x64-Release/mariadb_config/mariadb_config.c
@@ -118,7 +118,7 @@ void usage(void)
   puts("Copyright 2011-2020 MariaDB Corporation AB");
   puts("Get compiler flags for using the MariaDB Connector/C.");
   printf("Usage: %s [OPTIONS]\n", mariadb_progname);
-  printf("Compiler: Clang 17.0.2\n");
+  printf("Compiler: Clang 18.1.6\n");
   while (long_options[i].name)
   {
     if (!long_options[i].has_arg)
